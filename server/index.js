c6e52c5dd8188a570c77e52bf071a0d72bfdd66f
Ankit Singh <97746093+ankit-024@users.noreply.github.com>
2024-12-29 23:35:46 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin:"https://study-notion-core.vercel.app/",
+		origin:"https://study-notion-core.vercel.app",
 		credentials:true,
 	})
 )
